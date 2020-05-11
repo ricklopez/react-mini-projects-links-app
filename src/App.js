cb8ab86587e4b71eb9f3f3cb15e0eaea0e0806d9
rick <rick@ricklopez.io>
2020-05-10 20:31:14 -0400

    [Convert to clas]

--- src/App.js
+++ src/App.js
@@ -4,36 +4,46 @@ import './App.css';
 import Nav from './Nav';
 import Actions from './Actions';
 
-function App() {
-  return (
-    <div className="App">
-      <div className="container">
-      <div className="row">
-        <div className="col">
-           <Nav time={Date.now()}/>
-           <Actions/>
-              <div className="links-list  my-3">
-                <h4 className="display-4">My Links</h4>
-                <ul className="list-group">
-                  <li className="list-group-item d-flex justify-content-between align-items-center link-item">
-                    Cras justo odio
-                    <span className="badge badge-primary badge-pill">14</span>
-                  </li>
-                  <li className="list-group-item d-flex justify-content-between align-items-center link-item">
-                    Dapibus ac facilisis in
-                    <span className="badge badge-primary badge-pill">2</span>
-                  </li>
-                  <li className="list-group-item d-flex justify-content-between align-items-center link-item">
-                    Morbi leo risus
-                    <span className="badge badge-primary badge-pill">1</span>
-                  </li>
-                </ul>
-              </div>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+  
+  render(){
+    
+    return (
+      <div className="App">
+        <div className="container">
+        <div className="row">
+          <div className="col">
+             <Nav time={Date.now()}/>
+             <Actions/>
+                <div className="links-list  my-3">
+                  <h4 className="display-4">My Links</h4>
+                  <ul className="list-group">
+                    <li className="list-group-item d-flex justify-content-between align-items-center link-item">
+                      Cras justo odio
+                      <span className="badge badge-primary badge-pill">14</span>
+                    </li>
+                    <li className="list-group-item d-flex justify-content-between align-items-center link-item">
+                      Dapibus ac facilisis in
+                      <span className="badge badge-primary badge-pill">2</span>
+                    </li>
+                    <li className="list-group-item d-flex justify-content-between align-items-center link-item">
+                      Morbi leo risus
+                      <span className="badge badge-primary badge-pill">1</span>
+                    </li>
+                  </ul>
+                </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+    
+  }
+ 
 }
 
 export default App;
