3661509bb0fd52161dda53ba6ad4a6591c1ef9e5
rick <rick@ricklopez.io>
2020-08-24 21:04:38 -0400

    save

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { render } from '@testing-library/react';
 import renderer from 'react-test-renderer';
 import App from './App';
 
+
 test('renders learn react Nav and List Item', () => {
-  const component = renderer.create(<App />);
+  const component = renderer.create(<App/>);
                                   
   let tree = component.toJSON();
   expect(tree).toMatchSnapshot();
                                
+});
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
 });
